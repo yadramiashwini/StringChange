9ad2a3510e2fe1be90c960d7408fb5e7fe6fab42
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-10 22:16:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace StringChange
+{
+    public class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter string:");
+            string str = Console.ReadLine().ToLower();
+            string output = " ";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    output += char.ToUpper(str[i]);
+                }
+                else
+                {
+                    output += char.ToLower(str[i]);
+                }
+            }
+            Console.WriteLine(output);
+
+        }
+    }
+}
